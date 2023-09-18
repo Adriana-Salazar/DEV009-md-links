5b3c0935918822137ddadbacc93e7fc1fec26877
Adriana-Salazar <adrianabsalazar@gmail.com>
2023-09-18 12:30:00 -0500

    se optimiza la función mdLinks para envolver todo el contenido en una promesa y no que se arme la promesa al final de la función como estaba

--- index.js
+++ index.js
@@ -1,82 +1,81 @@
 const fs = require("fs");
 const path = require("path");
 const md = require("markdown-it")();
-const { parse } = require("node-html-parser"); // Librería para parsear HTML
-const {
-  isMarkdownFile,
-  readingFile,
-  validateLinks,
-  readdirFiles,
-} = require("./data.js");
+const { parse } = require("node-html-parser");
+const { isMarkdownFile, readingFile, validateLinks, readdirFiles } = require("./data.js");
 
 function mdLinks(directoryPath, validate) {
-  const absolutePath = path.resolve(directoryPath);
-  if (!fs.existsSync(absolutePath)) {
-    return Promise.reject(new Error("La ruta no existe"));
-  }
-
-  const { files, directories } = readdirFiles(absolutePath);
-
-  const linksPromises = [];
-
-  for (const filePath of files) {
-    const fullFilePath = path.join(absolutePath, filePath);
-
-    if (isMarkdownFile(fullFilePath)) {
-      const linkPromise = readingFile(fullFilePath)
-        .then((fileContent) => {
-          const htmlContent = md.render(fileContent);
-          const root = parse.parse(htmlContent);
-
-          const links = root.querySelectorAll("a").map((anchor) => ({
-            href: anchor.getAttribute("href"),
-            text: anchor.text,
-            file: fullFilePath,
-          }));
-
-          if (validate) {
-            return Promise.all(
-              links.map((link) =>
-                validateLinks(link.href)
-                  .then((validationResult) =>
-                    Object.assign(link, validationResult)
-                  )
-                  .catch(() => {
-                    link.status = "Error";
-                    link.ok = "fail";
-                    return link;
-                  })
-              )
-            );
-          }
-
-          return links;
-        })
-        .catch((error) => {
-          console.error(`Error al procesar ${fullFilePath}: ${error.message}`);
-          return [];
-        });
-
-      linksPromises.push(linkPromise);
-    } else {
-      console.error(`Error: ${fullFilePath} no es un archivo Markdown`);
+  return new Promise((resolve, reject) => {
+    const absolutePath = path.resolve(directoryPath);
+    if (!fs.existsSync(absolutePath)) {
+      reject(new Error("La ruta no existe"));
+      return;
     }
-  }
 
-  for (const subdirectory of directories) {
-    const subdirectoryPath = path.join(absolutePath, subdirectory);
-    const subdirectoryLinks = mdLinks(subdirectoryPath, validate);
-    linksPromises.push(subdirectoryLinks);
-  }
+    const { files, directories } = readdirFiles(absolutePath);
 
-  return Promise.all(linksPromises).then((results) => {
-    const allLinks = [];
-    for (const result of results) {
-      allLinks.push(...result);
+    const linksPromises = [];
+
+    for (const filePath of files) {
+      const fullFilePath = path.join(absolutePath, filePath);
+
+      if (isMarkdownFile(fullFilePath)) {
+        const linkPromise = readingFile(fullFilePath)
+          .then((fileContent) => {
+            const htmlContent = md.render(fileContent);
+            const root = parse(htmlContent);
+
+            const links = root.querySelectorAll("a").map((anchor) => ({
+              href: anchor.getAttribute("href"),
+              text: anchor.text,
+              file: fullFilePath,
+            }));
+
+            if (validate) {
+              return Promise.all(
+                links.map((link) =>
+                  validateLinks(link.href)
+                    .then((validationResult) =>
+                      Object.assign(link, validationResult)
+                    )
+                    .catch(() => {
+                      link.status = "Error";
+                      link.ok = "fail";
+                      return link;
+                    })
+                )
+              );
+            }
+            return links;
+          })
+          .catch((error) => {
+            console.error(`Error al procesar ${fullFilePath}: ${error.message}`);
+            return [];
+          });
+        linksPromises.push(linkPromise);
+      } else {
+        console.error(`Error: ${fullFilePath} no es un archivo Markdown`);
+      }
     }
-    return allLinks;
-    
+
+    for (const subdirectory of directories) {
+      const subdirectoryPath = path.join(absolutePath, subdirectory);
+      const subdirectoryLinks = mdLinks(subdirectoryPath, validate);
+      linksPromises.push(subdirectoryLinks);
+    }
+
+    Promise.all(linksPromises)
+      .then((results) => {
+        const allLinks = [];
+        for (const result of results) {
+          allLinks.push(...result);
+        }
+        resolve(allLinks);
+      })
+      .catch(reject);
   });
 }
 
 module.exports = mdLinks;
+
+
